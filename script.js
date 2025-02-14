d2e372901369a5d8322575cacd9bcbb9d1339eee
fawxxc <anastasiaptasenko01@gmail.com>
2025-02-14 12:09:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function sendMessage() {
+    const messageElement = document.getElementById('message');
+    const messages = [
+        "Ну чо когда вернёшь деньги",
+        "пук пук ",
+        "Гострі Картузи, Шелбі Іваннка Олександрівна - головний картуз Херсонської області",
+        "1918 год, Смольный, в кабинете Ленина, Горький сосет у вождя мирового пролетариата.Ленин, прищурясь, гладит Горького по голове и шепчет -И кто-же назвал тебя горьким, Сладенький ты мой."
+        
+    ];
+
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    messageElement.textContent = messages[randomIndex];
+}
